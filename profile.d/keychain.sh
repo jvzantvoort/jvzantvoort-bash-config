f53ae8eae987118a3867e3ec5f97c4cbe94c7471
John van Zantvoort <john@vanzantvoort.org>
2013-12-31 00:33:08 +0100

    fixed irritation with ssh keys

--- profile.d/keychain.sh
+++ profile.d/keychain.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
-/usr/bin/keychain --quiet id_dsa id_dsa_bertha id_dsa_upload id_rsa id_rsa_snow
-[ -z "$HOSTNAME" ] && HOSTNAME=‘uname -n‘
+
+[ -x /usr/bin/keychain ] || return
+
+# make sure the permissions of the keyfiles are c
+ls -1d $HOME/.ssh/*.pub|while read keyfile
+do
+  keyfile=$(basename $keyfile .pub)
+  if [ ! "$(stat -c "%a" "$HOME/.ssh/$keyfile")" = "600" ]
+  then
+    echo "(--) fix permissions for $keyfile"
+    chmod 600 "$HOME/.ssh/$keyfile"
+  fi
+done
+
+/usr/bin/keychain --quiet $(ls -1d $HOME/.ssh/*.pub | sed 's,.*\/\(.*\).pub,\1,'|sort | tr \\n ' ')
+
+[ -z "$HOSTNAME" ] && HOSTNAME=$(uname -n)
+
 [ -f $HOME/.keychain/$HOSTNAME-sh ] && . $HOME/.keychain/$HOSTNAME-sh
