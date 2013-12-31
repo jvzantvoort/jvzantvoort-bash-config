e494ac61891bf0dd5c941c7db153eeb50a050f3d
John van Zantvoort <john@vanzantvoort.org>
2013-12-31 05:29:19 +0100

    added etime function

--- profile.d/functions.sh
+++ profile.d/functions.sh
@@ -7,3 +7,7 @@
 #
 #==========================================================================
 
+etime()
+{
+    perl -MPOSIX -e 'print POSIX::strftime( "%A %B %d %Y %H:%M:%S\n", localtime( shift(@ARGV) ) );' $1
+}
