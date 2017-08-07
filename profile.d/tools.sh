c9a46e098777e5b64bff3a53d90458055c8dae16
John van Zantvoort <john@vanzantvoort.org>
2017-08-07 17:17:53 +0200

    update tools

--- profile.d/tools.sh
+++ profile.d/tools.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+#===============================================================================
+#
+#         FILE:  tools.sh
+#
+#        USAGE:  tools.sh
+#
+#  DESCRIPTION:  tools dir
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  John van Zantvoort (jvzantvoort), John@vanZantvoort.org
+#      COMPANY:  none
+#      CREATED:  07-Aug-2017
+#===============================================================================
+[[ -d "$HOME/.tools/bin" ]] || return
+
 export PATH="$PATH:$HOME/.tools/bin"
