f32320604ccce5b284d0198c951007e0667f1565
John van Zantvoort <john@vanzantvoort.org>
2017-08-07 22:45:45 +0200

    added cprint, reorder load order, ensure no duplication

--- bashrc.sh
+++ bashrc.sh
@@ -39,12 +39,19 @@ __store_debug()
 
     case $ACTION in
       cleanup) rm "$HOME/.bashrc_configfiles" ;;
-      missing) echo "(!!) $FILENAME missing"; return ;;
+      missing) ${HOME}/.bash/bin/cprint nok "$FILENAME missing"; return ;;
       *) ;;
     esac
 
     echo "$FILENAME" >> "$HOME/.bashrc_configfiles"
-    echo "(++) $FILENAME sourced"
+    ${HOME}/.bash/bin/cprint debug "$FILENAME sourced"
+}
+
+function __in()
+{
+  local e
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+  return 1
 }
 
 # some internal variable definitions
@@ -52,17 +59,7 @@ __store_debug()
 if [ $OSNAME == "Linux" ]
 then
   OSNAMES[${#OSNAMES[*]}]="Linux"
-  if [ -e /etc/redhat-release ]
-  then
-
-    OSNAMES[${#OSNAMES[*]}]="RedHat"
-
-    if grep -qi centos /etc/redhat-release
-    then
-      OSNAMES[${#OSNAMES[*]}]="CentOS"
-    fi
-  fi
-
+  [[ -e /etc/redhat-release ]]    && OSNAMES[${#OSNAMES[*]}]="RedHat"
   [[ -e /etc/mandrake-release ]]  && OSNAMES[${#OSNAMES[*]}]="Mandrake"
   [[ -e /etc/SuSE-release ]]      && OSNAMES[${#OSNAMES[*]}]="SuSE"
   [[ -e /etc/fedora-release ]]    && OSNAMES[${#OSNAMES[*]}]="Fedora"
@@ -70,31 +67,34 @@ then
   [[ -e /etc/wrs-release ]]       && OSNAMES[${#OSNAMES[*]}]="WindRiver"
   [[ -e /etc/snow-release ]]      && OSNAMES[${#OSNAMES[*]}]="Snow"
 
+  if __in "RedHat" "${OSNAMES[@]}"
+  then
+    if [ -f "/etc/centos-release" ]
+    then
+      OSNAMES[${#OSNAMES[*]}]="CentOS"
+    elif grep -qi centos /etc/redhat-release
+    then
+      OSNAMES[${#OSNAMES[*]}]="CentOS"
+    fi
+  fi
+
   LSB_DIST=$(__check_lsb)
   if [ ! -z "$LSB_DIST" ]
   then
-    OSNAMES[${#OSNAMES[*]}]="$LSB_DIST"
+    if ! __in "$LSB_DIST" "${OSNAMES[@]}"
+    then
+      OSNAMES[${#OSNAMES[*]}]="$LSB_DIST"
+    fi
   fi
 
 else
   OSNAMES[${#OSNAMES[*]}]=$OSNAME
 fi
 
-OSNAMES[${#OSNAMES[*]}]=$SHN
-
 __store_debug "$HOME/.bashrc" "yes"
 
-for osname in $(seq 0 $((${#OSNAMES[@]} - 1)))
-do
-  FILENAME="${HOME}/.bash/platform.d/${OSNAMES[$osname]}.sh"
-  if [ -f "${FILENAME}" ]
-  then
-    __store_debug "$FILENAME"
-    . $FILENAME
-  else
-    __store_debug "$FILENAME" "missing"
-  fi
-done
+# print the motd in new screen sessions
+[[ -z "${STY}" ]] || cat /etc/motd
 
 for i in $HOME/.bash/profile.d/*.sh
 do
@@ -109,4 +109,21 @@ do
         fi
     fi
 done
+${HOME}/.bash/bin/cprint profile "common"
+
+for osname in $(seq 0 $((${#OSNAMES[@]} - 1)))
+do
+  FILENAME="${HOME}/.bash/platform.d/${OSNAMES[$osname]}.sh"
+  if [ -f "${FILENAME}" ]
+  then
+    __store_debug "$FILENAME"
+    . $FILENAME
+  else
+    __store_debug "$FILENAME" "missing"
+  fi
+done
+
+unset __check_lsb
+unset __store_debug
+unset __in
 
