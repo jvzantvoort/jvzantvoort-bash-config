04fada5278f9351209f6be2cd83ec66107306683
John van Zantvoort <john@vanzantvoort.org>
2017-08-07 21:19:39 +0200

    la

--- platform.d/workstation.sh
+++ platform.d/workstation.sh
@@ -15,11 +15,5 @@
 
 [ -z "$TTY_NAME" ] || echo -e " \033[0;32m*\033[0m hostname:workstation profile sourced"
 
-if [ -z "$SESSIONNAME" ]
-then
-  PS1="\[\033[0;36m\]\u@\h\[\033[0m\]/\[\033[0;36m\]centos\[\033[0m\] \T [\[\033[1;36m\]\w\[\033[0m\]]
-# "
-else
-  PS1="\[\033[0;36m\]\u@\h\[\033[0m\]/\[\033[0;36m\]centos\[\033[0m\] ${PROMPT_Yellow}${SESSIONNAME}${PROMPT_END} \T [\[\033[1;36m\]\w\[\033[0m\]]
-# "
-fi
+[[ -z "$SESSIONNAME" ]] && return
+
