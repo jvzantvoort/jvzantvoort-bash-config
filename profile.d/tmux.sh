43998f86d00611a3abe9ddd719af853c72c565c1
John van Zantvoort <john.van.zantvoort@proxy.nl>
2019-05-28 00:30:40 +0200

    minor update

--- profile.d/tmux.sh
+++ profile.d/tmux.sh
@@ -7,5 +7,12 @@ export SESSIONNAME=`tmux display-message -p '#S'`
 
 # screen specific config
 # --------------------------------------
-[[ -f "$HOME/.bash/tmux.d/${SESSIONNAME}.env" ]] || return
-source  "$HOME/.bash/tmux.d/${SESSIONNAME}.env"
+if [ -f "$HOME/.bash/tmux.d/${SESSIONNAME}.env" ]
+then
+  "$HOME/.bash/bin/cprint" oke "${SESSIONNAME} found"
+  source  "$HOME/.bash/tmux.d/${SESSIONNAME}.env"
+else
+  "$HOME/.bash/bin/cprint" nok "${SESSIONNAME} not found"
+
+  return
+fi
