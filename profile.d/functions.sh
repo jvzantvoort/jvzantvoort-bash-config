963f37adce4e3dbb3f4f7fc88cbd268b6439bd93
John van Zantvoort <john.van.zantvoort@proxy.nl>
2019-08-05 22:29:05 +0200

    added update function

--- profile.d/functions.sh
+++ profile.d/functions.sh
@@ -13,3 +13,15 @@ etime()
 {
     perl -MPOSIX -e 'print POSIX::strftime( "%A %B %d %Y %H:%M:%S\n", localtime( shift(@ARGV) ) );' $1
 }
+
+function update_home_git()
+{
+  for target in vim bash tools
+  do
+    [[ -d "${HOME}/.${target}" ]] || continue
+    pushd "${HOME}/.${target}" >/dev/null 2>&1
+    git pull --quiet origin master
+    popd >/dev/null 2>&1
+  done
+}
+
