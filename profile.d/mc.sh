9f524d8e12e79b6c8d3d550cba89c8b49d66a292
John van Zantvoort <john.van.zantvoort@proxy.nl>
2019-09-01 13:08:01 +0200

    update mc wrapper

--- profile.d/mc.sh
+++ profile.d/mc.sh
@@ -1,3 +1,6 @@
+# if the wrapper does not exist, do not continue
+[[ -f "~/.bash/mc/mc-wrapper.sh" ]] || return 0
+
 # Don't define aliases in plain Bourne shell
 [ -n "${BASH_VERSION}${KSH_VERSION}${ZSH_VERSION}" ] || return 0
 alias mc='. /usr/libexec/mc/mc-wrapper.sh'
